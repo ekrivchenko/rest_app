b385d7b2d28250c38510cbdcf3783d8cac4ab769
Alexandru <alex@aylanetworks.com>
2017-09-23 23:01:51 -0700

    Add POST

--- rest.py
+++ rest.py
@@ -1,4 +1,4 @@
-#Main executable module of the app
+#!/usr/bin/env python
 import xml.etree.ElementTree as ET
 import MySQLdb as db
 import json, collections, pymysql, web
@@ -10,15 +10,15 @@ urls = (
 )
 app = web.application(urls, globals())
 connection = pymysql.connect(host='testdb.chvxt94wiqg2.us-east-1.rds.amazonaws.com', port=3306, user='barmaley', password='barmaley', db='test_db')
+
 class get_user:
     def GET(self, user):
-        print user
-        sql = "SELECT * FROM users WHERE uuid = '%s'" % user 
+        sql = "SELECT * FROM users WHERE uuid = '%s'" % user
         cursor = connection.cursor()
         cursor.execute(sql)
         rows = cursor.fetchall()
         field_names = json_print(rows, cursor)
-	return field_names
+        return field_names
 class list_users:
     def GET(self):
         sql = "SELECT * FROM users"
@@ -29,12 +29,15 @@ class list_users:
         return field_names
 class add_user:
     def POST(self):
-        data = web.data()
-        add_user = ("INSERT INTO users "
-               "(uuid, first_name, last_name, email) "
-               "VALUES (%s, %s, %s, %s")     
-
-        return data
+        data = json.loads(web.data())
+        uuid, fn, ln, email = data["uuid"], data["first_name"], data["last_name"], data["email"]
+        add_user = 'INSERT INTO users (uuid, first_name, last_name, email) VALUES ("%s", "%s", "%s"," %s")' % (uuid, fn, ln, email)
+        cursor = connection.cursor()
+        cursor.execute(add_user)
+        connection.commit()
+        cursor.close()
+        connection.close()
+        return add_user
 def json_print(rows, cursor):
         field_names = [i[0] for i in cursor.description]
         objects_list = []
@@ -47,4 +50,3 @@ def json_print(rows, cursor):
         return json_string
 if __name__ == "__main__":
     app.run()
-
