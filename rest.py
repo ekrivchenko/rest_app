5d7362b046f84c3621d0f78c0d57ea381b6bcc0d
Eugene Krivchenko <eu.kriv@gmail.com>
2017-09-28 17:44:30 -0700

    fixing shit

--- rest.py
+++ rest.py
@@ -1,19 +1,17 @@
-#!/usr/bin/env python
-from bson import json_util
-import xml.etree.ElementTree as ET
-import MySQLdb as db
 import json, collections, pymysql, web
-from datetime import date, datetime
+from bson import json_util
+
 urls = (
     '/user', 'add_user',
     '/users', 'list_users',
     '/user/(.*)', 'get_user'
 )
 app = web.application(urls, globals())
-#connection = pymysql.connect(host='testdb.chvxt94wiqg2.us-east-1.rds.amazonaws.com', port=3306, user='barmaley', password='barmaley', db='test_db')
+
 class get_user:
     def GET(self, user):
-        connection = pymysql.connect(host='testdb.chvxt94wiqg2.us-east-1.rds.amazonaws.com', port=3306, user='barmaley', password='barmaley', db='test_db')
+        connection = pymysql.connect(host='testdb.chvxt94wiqg2.us-east-1.rds.amazonaws.com', port=3306,
+                                     user='barmaley', password='barmaley', db='test_db')
         sql = "SELECT * FROM users WHERE uuid = '%s'" % user
         cursor = connection.cursor()
         cursor.execute(sql)
@@ -22,9 +20,11 @@ class get_user:
         cursor.close()
         connection.close()
         return field_names
+
 class list_users:
     def GET(self):
-        connection = pymysql.connect(host='testdb.chvxt94wiqg2.us-east-1.rds.amazonaws.com', port=3306, user='barmaley', password='barmaley', db='test_db')
+        connection = pymysql.connect(host='testdb.chvxt94wiqg2.us-east-1.rds.amazonaws.com', port=3306,
+                                     user='barmaley', password='barmaley', db='test_db')
         sql = "SELECT * FROM users"
         cursor = connection.cursor()
         cursor.execute(sql)
@@ -33,23 +33,23 @@ class list_users:
         cursor.close()
         connection.close()
         return field_names
+
+
 class add_user:
     def POST(self):
-	connection = pymysql.connect(host='testdb.chvxt94wiqg2.us-east-1.rds.amazonaws.com', port=3306, user='barmaley', password='barmaley', db='test_db')
+        connection = pymysql.connect(host='testdb.chvxt94wiqg2.us-east-1.rds.amazonaws.com', port=3306,
+                                 user='barmaley', password='barmaley', db='test_db')
         data = json.loads(web.data())
         uuid, fn, ln, email = data["uuid"], data["first_name"], data["last_name"], data["email"]
         add_user = 'INSERT INTO users (uuid, first_name, last_name, email, data) VALUES ("%s", "%s", "%s"," %s", now())' % (uuid, fn, ln, email)
         cursor = connection.cursor()
         cursor.execute(add_user)
         connection.commit()
-#        sql = "SELECT * FROM users WHERE uuid = '%s'" % uuid
-#        cursor.execute(sql)
-#        rows = cursor.fetchall()
-#        field_names = json_print(rows, cursor)
         cursor.close()
         connection.close()
         return web.created("User %s created " % uuid)
-#        return field_names
+
+
 def json_print(rows, cursor):
         field_names = [i[0] for i in cursor.description]
         objects_list = []
